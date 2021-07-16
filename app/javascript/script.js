24cb968c9374a6f43f88d7c464cc8049f728423a
root-344 <michimasa8@gmail.com>
2021-07-17 01:39:18 +0900

    close-btn作成

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,8 +1,18 @@
 $(function () {
- $('#circle').click(function() {
-   $('#circle').fadeOut(0);
-   setTimeout(function() {
-    $('#upper-modal').slideUp(900);
-   }, 400);
- });
+  $('#circle').click(function() {
+    $('#circle').fadeOut(0);
+    setTimeout(function() {
+     $('#upper-modal').slideUp(900);
+    }, 400);
+    setTimeout(function() {
+      $('#close-btn').fadeIn(0);
+     }, 1000);
+  });
+  $('#close-btn').click(function() {
+    $('#close-btn').fadeOut(0);
+    $('#upper-modal').slideDown(600);
+    setTimeout(function() {
+      $('#circle').fadeIn(0)
+    }, 800);
+  });
 });
